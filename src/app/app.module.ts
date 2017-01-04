06921ef6d13baf948a473239d1b8947eb3d5d985
Mohitkh7 <mohitkh7@gmail.com>
2017-01-05 01:38:02 +0530

    Angular firebase included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+//Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+//AF2 settings
+export const firebaseConfig={
+  apiKey: "AIzaSyC1mVPrGCMgKELWOAKdWoQZ5I8oa--0Sgc",
+  authDomain: "indore-traffic-updates.firebaseapp.com",
+  databaseURL: "https://indore-traffic-updates.firebaseio.com",
+  storageBucket: "indore-traffic-updates.appspot.com",
+  messagingSenderId: "301590757882"
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,76 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
+import{ AngularFire, FirebaseListObservable } from 'angularfire2';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+	updates: FirebaseListObservable<any>;
+  constructor(public navCtrl: NavController, af: AngularFire, public alertCtrl: AlertController) {
+    this.updates=af.database.list('/updates');
+  }
+
+  addUpdate(){
+  	//getting present time as form of date
+  	let t=(new Date()).toISOString();
+  	//date=dt.toISOString();
+  	let prompt = this.alertCtrl.create({
+	    title: 'Traffic Updates ',
+	    message: "Enter details of traffic update ",
+	    inputs: [
+	      {
+	        name: 'title',
+	        placeholder: 'Title'
+	      },
+	      {
+	      	name: 'message',
+	      	placeholder: 'Message'
+	      },
+	      {
+	      	name: 'stripcolor',
+	      	placeholder: 'Strip Color i.e red, orange, yellow, green, blue'
+	      }
+	    ],
+	    buttons: [
+	      {
+	        text: 'Cancel',
+	        handler: data => {
+	          console.log('Cancel clicked');
+	        }
+	      },
+	      {
+	        text: 'Save',
+	        handler: data => {
+	          this.updates.push({
+	            title: data.title,
+	            message: data.message,
+	            stripcolor: data.stripcolor,
+	            time:t,
+	          });
+	        }
+	      }
+	    ]
+	  });
+  	prompt.present();
+	}
+  
+  //function to calculate time
+  showTime(t){
+
+  	//diff of minutes
+  	let oneMin = 60*1000;
+  	let msgT=new Date(t);
+  	let currT=new Date();
+  	let diff=Math.round((msgT.getTime() - currT.getTime())/(oneMin));
+  	if(Math.abs(diff)>59){
+  		diff=Math.round(diff/60);
+  		return Math.abs(diff)+" Hours Ago";
+  	}
+  	return Math.abs(diff) +" Minutes Ago";
   }
 
 }
